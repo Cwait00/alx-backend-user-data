7d4fd4ff9cdb8d26e5795ff4534d8a3420da9443
cwait00 <cwaitanobongoza@gmail.com>
2024-05-30 12:26:19 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,12 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
     """
     Returns the log message obfuscated.
 
