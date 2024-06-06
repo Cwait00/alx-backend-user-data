5ba9c2955d61961894ae5d008e921a1ae5ca25c0
cwait00 <cwaitanobongoza@gmail.com>
2024-06-06 16:03:43 +0000

    Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ Auth class for API authentication """
 from typing import List, TypeVar
 from flask import request
+import os
 
 
 class Auth:
@@ -34,3 +35,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method for current user """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
