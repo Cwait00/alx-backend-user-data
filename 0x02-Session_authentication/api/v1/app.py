cda3c09ed1e5436cd6140ef25f695377245c43ea
cwait00 <cwaitanobongoza@gmail.com>
2024-06-07 17:16:05 +0000

    -a

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,11 +2,10 @@
 """
 Route module for the API
 """
-
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -24,12 +23,6 @@ elif AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-elif AUTH_TYPE == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif AUTH_TYPE == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -55,24 +48,17 @@ def before_request() -> None:
     """ Method to handle before request processing """
     if auth is None:
         return
-
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
+        '/api/v1/forbidden/'
     ]
-
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
+    if auth.authorization_header(request) is None:
         abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
-
     request.current_user = auth.current_user(request)
 
 
