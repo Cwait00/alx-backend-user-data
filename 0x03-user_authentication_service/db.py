0f58bf62472f73b9b68a2cfc439d2f55ac94cf84
cwait00 <cwaitanobongoza@gmail.com>
2024-06-11 11:46:04 +0000

    -a

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,17 +6,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class for interacting with the database"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -24,17 +24,43 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database and return the User object.
-        """
+        """Add a new user to the database and return the User object"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user by arbitrary keyword arguments and return the User object.
+        Raise NoResultFound if no user is found.
+        Raise InvalidRequestError if query argument is invalid.
+        """
+        query = self._session.query(User)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise InvalidRequestError
+            query = query.filter(getattr(User, key) == value)
+        user = query.first()
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user's attributes.
+        Raise ValueError if attribute is invalid.
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise ValueError(f"Invalid attribute: {key}")
+            setattr(user, key, value)
+        self._session.commit()
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,25 +1,69 @@
 #!/usr/bin/env python3
 """
-Main file
+Main file for testing DB module functionalities
 """
 from user import User
 from db import DB
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
-def print_user_table_info():
+
+def print_user_table_info() -> None:
+    """Print information about the User table"""
     print(User.__tablename__)
-
     for column in User.__table__.columns:
         print("{}: {}".format(column, column.type))
 
-def test_add_user():
-    my_db = DB()
 
+def test_add_user() -> None:
+    """Test the add_user method of DB class"""
+    my_db = DB()
     user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
     print(user_1.id)
 
     user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
     print(user_2.id)
 
+
+def test_find_user_by() -> None:
+    """Test the find_user_by method of DB class"""
+    my_db = DB()
+    user = my_db.add_user("test@test.com", "PwdHashed")
+    print(user.id)
+
+    find_user = my_db.find_user_by(email="test@test.com")
+    print(find_user.id)
+
+    try:
+        find_user = my_db.find_user_by(email="test2@test.com")
+        print(find_user.id)
+    except NoResultFound:
+        print("Not found")
+
+    try:
+        find_user = my_db.find_user_by(no_email="test@test.com")
+        print(find_user.id)
+    except InvalidRequestError:
+        print("Invalid")
+
+
+def test_update_user() -> None:
+    """Test the update_user method of DB class"""
+    my_db = DB()
+    email = 'test@test.com'
+    hashed_password = "hashedPwd"
+    user = my_db.add_user(email, hashed_password)
+    print(user.id)
+
+    try:
+        my_db.update_user(user.id, hashed_password='NewPwd')
+        print("Password updated")
+    except ValueError:
+        print("Error")
+
+
 if __name__ == "__main__":
     print_user_table_info()
     test_add_user()
+    test_find_user_by()
+    test_update_user()
