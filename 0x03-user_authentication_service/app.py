d2473ba7ab9c2cc0f87accf037208f89bff12d2a
cwait00 <cwaitanobongoza@gmail.com>
2024-06-12 14:13:29 +0000

    updt

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,43 +2,17 @@
 """
 Basic Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from auth import Auth
-import logging
 
 app = Flask(__name__)
 
-# Instantiate the Auth object
 AUTH = Auth()
 
-# Suppress SQLAlchemy logging
-logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
-
-
 @app.route("/", methods=["GET"])
-def index() -> Flask.response_class:
-    """
-    Return a JSON payload with a welcome message
-    """
+def index():
+    """Return a JSON payload"""
     return jsonify({"message": "Bienvenue"})
-
-
-@app.route("/users", methods=["POST"])
-def users() -> Flask.response_class:
-    """
-    Register a new user
-    """
-    email: str = request.form.get('email')
-    password: str = request.form.get('password')
-
-    if not email or not password:
-        return jsonify({"message": "email and password required"}), 400
-
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
